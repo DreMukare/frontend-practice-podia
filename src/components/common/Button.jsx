54f8659d080b7822b2ec4f42395b1c0f9b46b2ae
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-08-08 20:01:46 +0300

    feat: created reusable button component

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,23 @@
+import theme from "../../utils/theme";
+
+const Button = (props) => {
+  const { text, handleClick } = props;
+
+  return (
+    <button
+      style={{
+        border: "none",
+        backgroundColor: theme.colors.purple,
+        padding: "1em 2em",
+        fontSize: "0.8em",
+        color: theme.colors.eggShell,
+        borderRadius: "25px",
+      }}
+      onClick={() => handleClick()}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
