1ece08a0d2bf5a34ad857012e8992bd1d8a13685
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-08-08 20:02:04 +0300

    refactor: improved theme object schema

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,12 @@
-export const theme = {
-  eggShell: "#fdfbf9",
-  white: "#ffffff",
-  purple: "#4b2aad",
-  lightPurple: "#9484c6",
-  cream: "#f5efeb",
+const theme = {
+  colors: {
+    eggShell: "#fdfbf9",
+    white: "#ffffff",
+    purple: "#4b2aad",
+    lightPurple: "#9484c6",
+    cream: "#f5efeb",
+    darkGrey: "#333333",
+  },
 };
+
+export default theme;
